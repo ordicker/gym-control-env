37f23e43e500d4c0827fb35593570dd929b7dbf7
Or Dicker <or.dicker@gmail.com>
2020-03-15 22:49:27 +0200

    copy pendulum env from original gym

--- gym_control_env/__init__.py
+++ gym_control_env/__init__.py
@@ -3,3 +3,10 @@
 __version__ = '0.1.0'
 __author__ = 'Or Dicker <or.dicker@gmail.com>'
 __all__ = []
+
+from gym.envs.registration import register
+
+register(
+    id='foo-v0',
+    entry_point='gym_control_env.envs:MyPendulumEnv',
+)
---
+++ gym_control_env/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_control_env.envs.pendulum import MyPendulumEnv
